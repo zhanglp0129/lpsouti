33efb5c66ffac6e70437dc06bdc151d6c9d89dbe
zhanglp0129 <zhanglp0129@gmail.com>
2024-11-10 15:21:01 +0800

    优化ip工具类

--- common/src/main/java/com/lpsouti/common/utils/IpUtil.java
+++ common/src/main/java/com/lpsouti/common/utils/IpUtil.java
@@ -11,7 +11,7 @@ public class IpUtil {
     private static final String LOCAL_IP = "127.0.0.1";
     /**
      * 获取IP地址
-     *
+     * <p>
      * 使用Nginx等反向代理软件， 则不能通过request.getRemoteAddr()获取IP地址
      * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
      */
@@ -84,12 +84,11 @@ public class IpUtil {
 
     /**
      * 将IPv4地址转换成字节
-     *IPv4地址
      * @param text
      * @return byte 字节
      */
     public static byte[] textToNumericFormatV4(String text) {
-        if (text.length() == 0) {
+        if (StrUtil.isEmpty(text)) {
             return null;
         }
 
