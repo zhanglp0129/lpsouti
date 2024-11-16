856d2421a7ec8a262305950776f9cb4da80c860b
zhanglp0129 <zhanglp0129@gmail.com>
2024-11-16 20:35:12 +0800

    将刷新免费余额改为使用悲观锁

--- common/src/main/java/com/lpsouti/common/utils/BalanceUtil.java
+++ common/src/main/java/com/lpsouti/common/utils/BalanceUtil.java
@@ -24,7 +24,7 @@ public class BalanceUtil {
     private final BaseMapper<Balance> balanceMapper;
 
     /**
-     * 刷新免费余额，会加上乐观锁
+     * 刷新免费余额。会加上悲观锁
      *
      * @param userId 用户id
      * @return 是否刷新成功
@@ -34,13 +34,18 @@ public class BalanceUtil {
         // TODO 测试该方法
         // 构建查询条件
         LambdaQueryWrapper<Balance> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.select(Balance::getId, Balance::getRefreshDate, Balance::getUpdateTime)
-                .eq(Balance::getUserId, userId);
+        queryWrapper
+                .select(Balance::getId, Balance::getRefreshDate, Balance::getFreeBalance)   // 指定查询字段
+                .eq(Balance::getUserId, userId) // 指定用户id
+                .last("for update");    // 使用悲观锁
         // 查询数据
         Balance balance = balanceMapper.selectOne(queryWrapper);
         log.debug("balance = {}, now date = {}", balance, LocalDate.now());
-        // 没有查到数据或今天已经刷新过了
-        if (balance == null || (balance.getRefreshDate() != null && balance.getRefreshDate().isEqual(LocalDate.now()))) {
+        // 没有查到数据、今天已经刷新过了、刷新后余额变少
+        if (balance == null ||
+                (balance.getRefreshDate() != null && balance.getRefreshDate().isEqual(LocalDate.now())) ||
+                balanceProperties.getDailyFreeBalance().compareTo(balance.getFreeBalance()) <= 0
+        ) {
             return false;
         }
 
@@ -48,8 +53,7 @@ public class BalanceUtil {
         LambdaUpdateWrapper<Balance> updateWrapper = new LambdaUpdateWrapper<>();
         updateWrapper.set(Balance::getFreeBalance, balanceProperties.getDailyFreeBalance()) // 修改免费余额
                 .set(Balance::getRefreshDate, LocalDate.now())  // 修改刷新日期
-                .eq(Balance::getId, balance.getId())    // 指定余额id
-                .eq(Balance::getUpdateTime, balance.getUpdateTime());   // 乐观锁
+                .eq(Balance::getId, balance.getId());   // 指定余额id
         return balanceMapper.update(updateWrapper) != 0;
     }
 }
