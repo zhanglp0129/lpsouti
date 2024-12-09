0159e64a102d747255e2baff1a6020f944e96432
zhanglp0129 <zhanglp0129@gmail.com>
2024-12-09 10:50:23 +0800

    完成修改题目对于题目类型的校验

--- admin/src/main/java/com/lpsouti/admin/controller/QuestionController.java
+++ admin/src/main/java/com/lpsouti/admin/controller/QuestionController.java
@@ -29,6 +29,16 @@ public class QuestionController {
 
     @PutMapping
     public ResultVO<Void> edit(@RequestBody @Valid QuestionEditDTO dto) {
+        // 检查答案类型与题目类型
+        // 未修改题目类型
+        boolean notChangeType = dto.getType() == null && dto.getAnswerContent() == null;
+        // 修改后的题目类型相同
+        boolean typeEqual = dto.getType() != null && dto.getAnswerContent() != null &&
+                dto.getType().compareTo(dto.getAnswerContent().getType()) == 0;
+        if (!notChangeType && !typeEqual) {
+            return ResultVO.error("题目类型错误");
+        }
+
         log.info("question edit dto = {}", dto);
         questionService.edit(dto);
         return ResultVO.success();
